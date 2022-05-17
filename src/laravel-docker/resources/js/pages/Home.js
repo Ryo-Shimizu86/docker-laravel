86d715bab4252d204971ae1d17ba320a1c6ea35e
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-05-17 18:36:00 +0900

    added front registration process

--- src/laravel-docker/resources/js/pages/Home.js
+++ src/laravel-docker/resources/js/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Button, Card } from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import MainTable from "../components/MainTable";
+import axios from "axios";
+import PostFrom from "../components/PostFrom";
 
 // スタイルの定義
 const useStyles = makeStyles((theme) =>
@@ -24,6 +25,9 @@ function Home() {
     // postsの状態を管理する
     const [posts, setPosts] = useState([]);
 
+    // フォームの入力値を管理するステートの定義
+    const [formData, setFormData] = useState({ name: "", content: "" });
+
     // 画面に到着したらgetPostsDataを呼ぶ
     useEffect(() => {
         getPostsData();
@@ -39,6 +43,38 @@ function Home() {
             })
             .catch(() => {
                 console.log("通信に失敗しました");
+            });
+    };
+
+    // 入力されたら（都度）入力値を変更するためのfunction
+    const inputChange = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        formData[key] = value;
+        let data = Object.assign({}, formData);
+        setFormData(data);
+    };
+
+    const createPost = async () => {
+        // 空だと弾く
+        if (formData == "") {
+            return;
+        }
+        // 入力値を投げる
+        await axios
+            .post("/api/post/create", {
+                name: formData.name,
+                content: formData.content,
+            })
+            .then((res) => {
+                // 戻り値をtodosにセット
+                const tempPosts = posts;
+                tempPosts.push(res.data);
+                setPosts(tempPosts);
+                setFormData("");
+            })
+            .catch((error) => {
+                console.log(error);
             });
     };
 
@@ -69,6 +105,13 @@ function Home() {
                 <div className="col-md-10">
                     <div className="card">
                         <h1>タスク管理</h1>
+                        <Card className={classes.card}>
+                            <PostFrom
+                                data={formData}
+                                inputChange={inputChange}
+                                btnFunc={createPost}
+                            />
+                        </Card>
                         <Card className={classes.card}>
                             {/* テーブル部分の定義 */}
                             <MainTable headerList={headerList} rows={rows} />
